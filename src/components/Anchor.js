e859b659f613a09be607d45f4738e0d430f7d3ca
Cezar Neaga <hi@cezarneaga.eu>
2019-10-04 16:49:15 +0200

    add title to Anchor component

--- src/components/Anchor.js
+++ src/components/Anchor.js
@@ -2,13 +2,20 @@ import React from 'react'
 import PropTypes, { any } from 'prop-types'
 import { Link } from 'gatsby'
 
-export const Anchor = ({ href, children, color, className, style }) => {
+export const Anchor = ({ href, children, title, color, className, style }) => {
   return href.startsWith('/') ? (
     <Link to={href} color={color} className={className} style={style}>
       {children}
     </Link>
   ) : (
-    <a href={href} color={color} className={className} style={style} target="_blank" rel="noopener noreferrer">
+    <a
+      href={href}
+      color={color}
+      className={className}
+      style={style}
+      target="_blank"
+      rel="noopener noreferrer"
+      title={title}>
       {children}
     </a>
   )
@@ -17,6 +24,7 @@ export const Anchor = ({ href, children, color, className, style }) => {
 Anchor.propTypes = {
   href: PropTypes.string.isRequired,
   children: any,
+  title: PropTypes.string,
   color: PropTypes.string,
   className: PropTypes.string,
 }
